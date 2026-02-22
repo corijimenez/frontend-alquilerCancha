35b2ede566ee24e7e50a9e2bcf5366f29e7e2264
ValeS <valeriastakelun@gmail.com>
2026-02-21 22:28:25 -0300

    agregue la estructura inicial para desarrollar el footer

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -1,7 +1,24 @@
+import { Col, Container, Row } from "react-bootstrap";
+
 const Footer = () => {
   return (
-    <footer className="text-center text-light py-4 bg-primary">
-      <p>&copy; Todos los derechos reservados.</p>
+    <footer className="bg-negro-brand text-white text-center p-3 mt-5">
+      <Container>
+      <Row className="gy-3 justify-content-between">
+        <Col md={4} className="d-flex flex-column align-items-center align-items-md-start">
+        <p>hola</p>
+        </Col>
+         <Col md={4} className="d-flex flex-column align-items-center align-items-md-start">
+        <p>hola</p>
+        </Col>
+         <Col md={4} className="d-flex flex-column align-items-center align-items-md-start">
+        <p>hola</p>
+        </Col>
+
+      </Row>
+
+      </Container>
+      <p>&copy; 2026 Rolling Canchas – Alquiler de Canchas</p>
     </footer>
   );
 };
