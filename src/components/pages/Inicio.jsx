5d4b72ded1c96328245cab8f2d3bb7bf2a117115
jonathan <jonathanfiorenza6@gmail.com>
2026-02-23 15:34:20 -0300

    agregue el stylo para las card de canchas y productos

--- src/components/pages/Inicio.jsx
+++ src/components/pages/Inicio.jsx
@@ -1,12 +1,14 @@
 import CarrouselInicio from "./CarrouselInicio";
 import BienvenidaInicio from "./BienvenidaInicio";
 import CanchasSeccion from "./CanchasSeccion";
+import ProductosInicio from "./ProductosInicio";
 const Inicio = () => {
     return (
         <main className="container my-4">
           <CarrouselInicio />
             <BienvenidaInicio />
               <CanchasSeccion />
+              <ProductosInicio />
         </main>
     );
 };
