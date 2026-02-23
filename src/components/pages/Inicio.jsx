31ff686b7b0c2bd7ac9db638f80ef6b7f9c1b033
jonathan <jonathanfiorenza6@gmail.com>
2026-02-23 15:22:01 -0300

    agregue el componente CanchasSeccion en Inicio.jsx

--- src/components/pages/Inicio.jsx
+++ src/components/pages/Inicio.jsx
@@ -1,10 +1,12 @@
 import CarrouselInicio from "./CarrouselInicio";
 import BienvenidaInicio from "./BienvenidaInicio";
+import CanchasSeccion from "./CanchasSeccion";
 const Inicio = () => {
     return (
         <main className="container my-4">
           <CarrouselInicio />
             <BienvenidaInicio />
+              <CanchasSeccion />
         </main>
     );
 };
