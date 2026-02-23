2cee0ac03b0715502aaabba677b829923f41cbc7
jonathan <jonathanfiorenza6@gmail.com>
2026-02-23 15:10:46 -0300

    agregue el componente bienvenido a la pagina de inicio

---
+++ src/components/pages/BienvenidaInicio.jsx
@@ -0,0 +1,16 @@
+import { Row, Col } from "react-bootstrap";
+
+export default function BienvenidaInicio() {
+  return (
+    
+    <Row className="mb-4 mt-5 align-items-center w-100 mx-0">
+      
+      <Col md={12} className="p-5 rounded bg-dark shadow-sm text-center">
+        <h2 className="display-5 fw-bold text-success">
+          Bienvenidos a FutbolProgramado
+        </h2>
+        
+      </Col>
+    </Row>
+  );
+}
