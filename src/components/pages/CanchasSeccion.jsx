2f8e167b801154bd67a6c57ca56bc03026f7eb72
jonathan <jonathanfiorenza6@gmail.com>
2026-02-23 15:17:36 -0300

    agregue el componente CanchasSeccion con datos de canchas

---
+++ src/components/pages/CanchasSeccion.jsx
@@ -0,0 +1,34 @@
+/*card de canchas*/
+import { Link } from "react-router-dom";
+
+const CanchasSeccion = () => {
+  // 1. Definimos los datos aquí mismo
+  const canchas = [
+    {
+      id: 1,
+      titulo: "Fútbol 5",
+      imagen: "/fulbito5.jpg",
+      descripcion: "Cancha de césped sintético ideal para 5 vs 5.",
+    },
+    {
+      id: 2,
+      titulo: "Fútbol 7",
+      imagen: "/futbol7.jpg",
+      descripcion: "Espacio amplio para partidos de 7 jugadores.",
+    },
+    {
+      id: 3,
+      titulo: "Fútbol 11",
+      imagen: "/futbol11.jpg",
+      descripcion: "Medidas reglamentarias para el máximo desafío.",
+    },
+  ];
+
+  return (
+    <>
+     
+    </>
+  );
+};
+
+export default CanchasSeccion;
