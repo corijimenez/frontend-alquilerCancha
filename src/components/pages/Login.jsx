e776abecb251b8efbd5d5feb93c46e048912dab2
basiliomartinez <basilioezequielmartinez@gmail.com>
2026-02-24 13:46:17 -0300

    feat: se creó la vista base de login

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,26 +1,7 @@
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
-
 const Login = () => {
   return (
     <main className="container my-4">
       <h1>Login</h1>
-      <Form>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-danger">error validacion</Form.Text>
-        </Form.Group>
-
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Contraseña</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-          <Form.Text className="text-danger">error validacion</Form.Text>
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Enviar
-        </Button>
-      </Form>
     </main>
   );
 };
