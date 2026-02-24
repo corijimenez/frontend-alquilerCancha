296bf0fbd9e3b60f5085f5ac0941b07ced3f8917
basiliomartinez <basilioezequielmartinez@gmail.com>
2026-02-24 13:51:42 -0300

    feat: se agregó el formulario de login con campos básicos

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,7 +1,26 @@
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+
 const Login = () => {
   return (
     <main className="container my-4">
       <h1>Login</h1>
+
+      <Form>
+        <Form.Group className="mb-3" controlId="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="email" placeholder="Ingresá tu email" />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="password">
+          <Form.Label>Contraseña</Form.Label>
+          <Form.Control type="password" placeholder="Ingresá tu contraseña" />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Enviar
+        </Button>
+      </Form>
     </main>
   );
 };
