cfb965b8623edaf8e9445429e400d95765759445
basiliomartinez <basilioezequielmartinez@gmail.com>
2026-02-27 12:11:12 -0300

    feat: se creó la vista base de administración de productos

---
+++ src/components/pages/AdminProductos.jsx
@@ -0,0 +1,12 @@
+const AdminProductos = () => {
+  return (
+    <main className="container my-4">
+      <h1 className="text-white fw-bold">Administrar Productos</h1>
+      <p className="text-white-50 mb-0">
+        Desde acá se gestionara el catálogo del ecommerce.
+      </p>
+    </main>
+  );
+};
+
+export default AdminProductos;
