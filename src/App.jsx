fd13d6b0749d213309603505d5994dabf06059eb
Lucas <lucasarbasualdo@gmail.com>
2026-02-28 20:16:18 -0300

    borre los cambios debido a un problema

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,8 @@ import Contacto from "./components/pages/Contacto";
 import QuienesSomos from "./components/pages/QuienesSomos";
 import ReservarCancha from "./components/pages/ReservarCancha";
 import ProtectorRutas from "./components/routes/ProtectorRutas";
-import Carrito from "./components/pages/Carrito";
+
+
 
 function App() {
   return (
@@ -24,7 +25,7 @@ function App() {
         <Route path="/contacto" element={<Contacto />} />
         <Route path="/nosotros" element={<QuienesSomos />} />
         <Route path="/reserva" element={<ReservarCancha />} />
-        <Route path="/carrito" element={<Carrito />} />
+
 
         {/* 🔒 Rutas protegidas */}
         <Route element={<ProtectorRutas />}>
