fde87d48e5d715efd32f3aa6651952c7cd84fffc
Gaston Gomez <gastongomez875@gmail.com>
2026-03-04 23:16:21 -0300

    agrego nuevo sistema de rutas protegidas para usuarios NO logueados, Logueados y Admin. el código fue probado previamente en un archivo de pruebas aíi que no hay más commits.

---
+++ src/components/routes/AdminProtector.jsx
@@ -0,0 +1,17 @@
+import { Navigate, Outlet } from "react-router-dom";
+
+const AdminProtector = () => {
+  const usuarioLogueado = JSON.parse(sessionStorage.getItem("usuarioKey")) || {};
+
+  if (!usuarioLogueado.token) {
+    return <Navigate to="/login" replace />;
+  }
+
+  if (usuarioLogueado.role !== "admin") {
+    return <Navigate to="/" replace />;
+  }
+
+  return <Outlet />;
+};
+
+export default AdminProtector
